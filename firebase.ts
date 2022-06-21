ebb643a807327eeceeb14e5a7b1761cc63796175
Sashank Shukla <ayaz.shukla@gmail.com>
2022-06-21 14:27:07 -0700

    tried firebase auth integ

--- firebase.ts
+++ firebase.ts
@@ -1,12 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDszE5OsNLPLh73PxacJ-L4d_9PC9f0fXw",
   authDomain: "res-track.firebaseapp.com",
   projectId: "res-track",
@@ -17,7 +16,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app:any = initializeApp(firebaseConfig);
-const auth:any = app.auth();
-
-export {auth};
+//const app:any = initializeApp(firebaseConfig);
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,9 +1,39 @@
 import { StyleSheet, Text, KeyboardAvoidingView, View, TextInput, Touchable, TouchableOpacity } from 'react-native'
 import {useState} from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {initializeApp} from 'firebase/app';
+import {firebaseConfig} from '../firebase';
+//import {auth} from '../firebase';
 
-const LoginScreen = () => {
+//initializeApp(firebaseConfig);
+
+const LoginScreen:React.FC = () => {
   const [email,setEmail] = useState<string>('');
   const [password,setPassword] = useState<string>('');
+
+//   const signUp = () => {
+//     const auth = getAuth();
+//     createUserWithEmailAndPassword(auth,email, password)
+//         .then((userCredential) => {
+//         const user = userCredential.user;
+//         console.log(user.email);
+//         })
+//         .catch((error) => {
+//         const errorMessage = error.message;
+//         console.log(errorMessage);
+//         });
+//     };
+
+//   const logIn = () => {
+//     auth
+//     .createUserWithEmailAndPassword(email,password)
+//     .then(userCredentials => {
+//         const user = userCredentials.user;
+//         console.log(user.email);
+//     })
+//     .catch(err => alert(err.message));
+//   };
+
   return (
     <KeyboardAvoidingView style= {styles.container} behavior="padding">
       <View style={styles.headerContainer}>
@@ -63,7 +93,7 @@ const styles = StyleSheet.create({
         width: '80%',
     },
     input: {
-        backgroundColor:'lightgray',
+        backgroundColor:'white',
         paddingHorizontal:15,
         paddingVertical:10,
         borderRadius:10,
@@ -88,13 +118,13 @@ const styles = StyleSheet.create({
         fontSize:16,
     },
     buttonOutline: {
-        backgroundColor:'#091c32',
+        backgroundColor:'#eab31a',
         marginTop:5,
-        borderColor:'lightblue',
+        borderColor:'#eab31a',
         borderWidth:3,
     },
     buttonOutlineText: {
-    color:'lightblue',
+    color:'#091c32',
     fontWeight:'700',
     fontSize:16,
     },
