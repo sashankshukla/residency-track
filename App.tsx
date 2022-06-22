262a446653092c0a9102848240babbf023a3805e
Sashank Shukla <ayaz.shukla@gmail.com>
2022-06-21 20:25:49 -0700

    added home screen

--- App.tsx
+++ App.tsx
@@ -11,9 +11,9 @@ const App = () => {
   return (
     <NavigationContainer>
        <Stack.Navigator>
+          <Stack.Screen options = {{headerShown: false}} name="Home" component={HomeScreen}/>
          <Stack.Screen  options = {{headerShown: false}} name="Login" component={LoginScreen}/>
-         <Stack.Screen  name="Main" component={MainScreen} />
-         <Stack.Screen  name="Home" component={HomeScreen} />
+         <Stack.Screen  options = {{headerShown: false}} name="Main" component={MainScreen}/>
        </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 
 const HomeScreen:React.FC = () => {
+  const navigator = useNavigation();
+  const switchScreen = () =>{
+     navigator.navigate("Login");
+  }
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>HomeScreen</Text>
+      <TouchableOpacity onPress={switchScreen}>
+        <Text style={styles.text}>Head to Login</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+   container:{
+    backgroundColor:'black',
+    color:'white',
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+   },
+   text : {
+    color:'white',
+   }
+})
